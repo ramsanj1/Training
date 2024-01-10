9683d2a29e7da83a2828bd38aeba576c7566da3a
ramsanj1 <62554361+ramsanj1@users.noreply.github.com>
2024-01-10 16:41:13 +0530

    Update main.tf

--- Modules/Terraform_linux_network/main.tf
+++ Modules/Terraform_linux_network/main.tf
@@ -19,3 +19,26 @@ resource "azurerm_subnet" "my_terraform_subnet" {
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+# create network security resource_group_location
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "terraformnsg"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "AllowSSH"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.my_terraform_nsg.name
+}
+
+output "subnet_id" {
+value = azurerm_subnet.my_terraform_subnet.id
+}
