b8b1f2b940a2ba2dd9289d072b65f3e1bb798c55
ramsanj1 <62554361+ramsanj1@users.noreply.github.com>
2024-01-10 16:31:03 +0530

    Update install_packages.sh

--- Modules/Terraform_linux_instance/install_script.sh
+++ Modules/Terraform_linux_instance/install_script.sh
@@ -5,3 +5,9 @@ sudo apt-get update
 
 # Install Java
 sudo apt-get install -y default-jre
+
+# Install ansible
+sudo apt install -y ansible
+
+# Set the script as executable
+chmod +x install_packages.sh
